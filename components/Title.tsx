525491b978eb965c3d4f50ecbbbd5062831ee395
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-20 23:06:36 -0300

    style: add styles to title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -6,6 +6,18 @@ type TitleProps = {
 
 export default function Title({ as, className, children }: TitleProps) {
   const Tag = as;
+  
+  const variants = {
+    h1: "text-[32px]",
+    h2: "text-3xl",
+    h3: "text-2xl",
+  };
 
-  return <Tag className={`${className}`}>{children}</Tag>;
+  return (
+    <Tag
+      className={`${className} ${variants[as]} text-center font-semibold text-darkGrayishBlue`}
+    >
+      {children}
+    </Tag>
+  );
 }
