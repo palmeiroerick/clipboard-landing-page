0de63db83a16596e5605afcfc19ee305808ec2e8
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-20 23:39:34 -0300

    style: add desktop styles to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({ type }: ButtonProps) {
 
   return (
     <button
-      className={`${variants[type]} h-12 w-80 rounded-full text-lg font-semibold text-[#fff]`}
+      className={`${variants[type]} h-12 w-80 rounded-full text-lg font-semibold text-[#fff] lg:w-56`}
     >
       Download for {type}
     </button>
