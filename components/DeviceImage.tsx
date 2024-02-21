0363114442c8ed4bcc32d0921167b017091b3195
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-20 23:39:44 -0300

    style: add desktop styles to deviceimage component

--- components/DeviceImage.tsx
+++ components/DeviceImage.tsx
@@ -7,7 +7,9 @@ type DeviceImageProps = {
 
 export default function DeviceImage({ src, className }: DeviceImageProps) {
   return (
-    <div className={`${className} relative h-[243.33px] w-[320px]`}>
+    <div
+      className={`${className} relative h-[243.33px] w-80 lg:h-[486.66px] lg:w-[640px]`}
+    >
       <Image className="object-contain" src={src} alt="device image" fill />
     </div>
   );
